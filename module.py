e8d9eb76ae18dd79eaa6a747fa790ba3cba64ba2
mdche001 <43078582+mdche001@users.noreply.github.com>
2018-09-15 09:41:59 +0800

    Update and rename module to module.py

---
+++ module.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+import tweepy
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print (tweet.text)

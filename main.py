56f98d382477e95fe26901e303a38c9185a0ffe9
mdche001 <mdche@bu.edu>
2018-09-22 13:23:16 +0800

    test main（）

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys
+import os
+
+import describe_images
+import twitterimage
+import generatevedio as generatevedio
+import describe_images1
+
+def main():
+    screen_name = sys.argv[1]
+    print (screen_name)
+    print ("*")
+    print ("downloading image from "+screen_name)
+    print ("*")
+    twitterimage.get_images(screen_name)
+    print ("*")
+    print ("analyzing video")
+    print ("*")
+    describe_images1.get_describe( screen_name)
+    print ("*")
+    print ("creating video from images"+screen_name)
+    print ("*")
+    generatevedio.create(screen_name)
+main()

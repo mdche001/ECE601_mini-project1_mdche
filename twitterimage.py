8753b54dbba232a3fcf18569b5f33ff199f27ed2
mdche001 <mdche@bu.edu>
2018-09-22 13:28:05 +0800

    Twitter wrap v0.9

--- twitterimage.py
+++ twitterimage.py
@@ -1,64 +1,56 @@
-#!/usr/bin/env python
-# encoding: utf-8
-# Author - Prateek Mehta
+import tweepy  # Ref:https://tweepy.readthedocs.io/en/v3.5.0/index.html
+import os, sys
+import wget
+
+#Authentication for using twitter API
+consumer_key = 'lklWiDCCsr4XrKfj8lHEv5vo3'
+consumer_secret = '7a50uiKUoJqjwjKdWOT6kbf9otrYTnsrCxJo56tB4bf1htZJyQ'
+access_token = '1038239626966822917-DOF0WSniAo3COib0JFFDqhHC3sNFhU'
+access_token_secret = 'GVsO61PvAanANCZFhTk4tgRdJaXGZv2UYglA0hbG5BdQG'
 
 
-import tweepy  # https://github.com/tweepy/tweepy
-import json
-
-# Twitter API credentials
-consumer_key = "lklWiDCCsr4XrKfj8lHEv5vo3"
-consumer_secret = "7a50uiKUoJqjwjKdWOT6kbf9otrYTnsrCxJo56tB4bf1htZJyQ"
-access_key = "1038239626966822917-DOF0WSniAo3COib0JFFDqhHC3sNFhU"
-access_secret = "GVsO61PvAanANCZFhTk4tgRdJaXGZv2UYglA0hbG5BdQG"
-
-
-def get_all_tweets(screen_name):
-    # Twitter only allows access to a users most recent 3240 tweets with this method
-
-    # authorize twitter, initialize tweepy
+def get_images(screen_name):
+    #set authentication
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_key, access_secret)
+    auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
+    # Receive the 1st set of user status
+    try:
+        tweets = api.user_timeline(screen_name, count=200)
+    except:
+        print ('error')
+        exit()
+    # Check if there are no more tweets
+    if (len(tweets) == 0):
+        print('No more tweets existed')
+    # Get url of jpg
+    else:
+        media_files = set()
+        for status in tweets:
+            media = status.entities.get('media', [])
+            if (len(media) > 0):
+                url = media[0]['media_url']
+                # find .jpg images
+                file_type = url.split(".")[-1]
+                if (file_type == "jpg"):
+                    media_files.add(media[0]['media_url'])
+        if (len(media_files) == 0):
+            print ("no more jpg images")
+            exit()
+    # creat a new folder for twitter images
+    directory = os.getcwd() + "/" + screen_name
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        else:
+            print ("already exsist")
+    except:
+        print('Error')
+    # Download images into the folder
+    i=0
+    for url in media_files:
+        image_name = directory + '/image'+str(i) +".jpg"
+        image=wget.download(url, out= image_name)
+        i += 1
 
-    # initialize a list to hold all the tweepy Tweets
-    alltweets = []
 
-    # make initial request for most recent tweets (200 is the maximum allowed count)
-    new_tweets = api.user_timeline(screen_name=screen_name, count=10)
-
-    # save most recent tweets
-    alltweets.extend(new_tweets)
-
-    # save the id of the oldest tweet less one
-    oldest = alltweets[-1].id - 1
-
-    # keep grabbing tweets until there are no tweets left to grab
-    while len(new_tweets) > 0:
-
-        # all subsiquent requests use the max_id param to prevent duplicates
-        new_tweets = api.user_timeline(screen_name=screen_name, count=10, max_id=oldest)
-
-        # save most recent tweets
-        alltweets.extend(new_tweets)
-
-        # update the id of the oldest tweet less one
-        oldest = alltweets[-1].id - 1
-        if (len(alltweets) > 15):
-            break
-        print ("...%s tweets downloaded so far" % (len(alltweets)))
-
-    # write tweet objects to JSON
-    file = open('tweet.json', 'w')
-    print ("Writing tweet objects to JSON please wait...")
-    for status in alltweets:
-        json.dump(status._json, file, sort_keys=True, indent=4)
-
-    # close the file
-    print ("Done")
-    file.close()
-
-
-if __name__ == '__main__':
-    # pass in the username of the account you want to download
-    get_all_tweets("@Ibra_official")

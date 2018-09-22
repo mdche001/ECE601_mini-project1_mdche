80af6cc5a57eff78ed148951af9750a3c0316382
mdche001 <mdche@bu.edu>
2018-09-23 04:34:17 +0800

    solve  label overlap

--- describe_images1.py
+++ describe_images1.py
@@ -9,8 +9,13 @@ client = vision.ImageAnnotatorClient()
 
 
 def get_describe( screen_name):
-    for i in range(23):
-        if os.path.exists('./'+screen_name+'/'+'image'+ str(i) + '.jpg') == True:
+    '''
+    list = os.listdir('C:/User/synox/mini_project1/'+screen_name+'/')
+    for i in (0, len(list))                                            P:  cannot find images in screen_name/
+    '''
+    i=0
+    while(True):
+        if os.path.exists('./'+screen_name+'./'+'image'+ str(i) + '.jpg') == True:
             file_name = os.path.join(os.path.dirname('C:/Users/synox/mini_project1/'+screen_name+'/image%s.jpg'), 'image%s.jpg'%(i))
             with io.open(file_name, 'rb') as image_file:
                 content = image_file.read()
@@ -21,12 +26,16 @@ def get_describe( screen_name):
             response = client.label_detection(image=image)
             labels = response.label_annotations
 
+            j=0
             for label in labels:
                 raw_image = Image.open(file_name)
-                draw = ImageDraw.Draw(raw_image)  # 修改图片
-                font = ImageFont.truetype('C:/Windows/Fonts/Tahoma/tahoma.ttf', 36) #P:OSError: cannot open resource  wrong path to .ttf
-                draw.text((100, 40), label.description, fill=(255,255,0+5), font=font)
+                draw = ImageDraw.Draw(raw_image)  #
+                font = ImageFont.truetype('C:/Windows/Fonts/Tahoma/tahoma.ttf', 15) #P:OSError: cannot open resource  wrong path to .ttf
+                draw.text((10, 10+15*j), label.description, fill=(255,255,0), font=font)#P:  labels overlap on the same position
+                j=j+1
                 raw_image.save(file_name)
-                break
+            i = i+1
+
         else:
-            print('no file existed')
+            print('no more jpg existed')
+            break

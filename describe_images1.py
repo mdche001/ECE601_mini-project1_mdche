9159bcd1e97ee637a2983129e6fd36ba7c933203
mdche001 <mdche@bu.edu>
2018-12-01 13:13:22 +0800

    script connect to mysql

--- describe_images1.py
+++ describe_images1.py
@@ -3,11 +3,20 @@ import io
 from google.cloud import vision
 from google.cloud.vision import types
 from PIL import ImageDraw, Image, ImageFont
+import pymongo
+import mysql.connector
+import datetime
 
+def get_describe(screen_name, username, img_num):
 
-def get_describe( screen_name):
+    # connect database
+    mydb = mysql.connector.connect(host="localhost", user="root", passwd="123456", auth_plugin='mysql_native_password')
+    mycursor = mydb.cursor()
+    mycursor.execute("use mini_project3_db")
+    sql = "INSERT INTO user_data (username,img_num,description,description_num,file_name,Modified_time) VALUES (%s,%s,%s,%s,%s,%s)"
+
     client = vision.ImageAnnotatorClient()
-    # list = os.listdir('C:/User/synox/mini_project1/'+screen_name+'/')
+    # list = os.listdir('C:/User/synox/mini_project1/'+screen_name+'/
     # for i in (0, len(list))  P:  I cannot find images in screen_name/ by this method
     i=0
     while(True):
@@ -25,7 +34,11 @@ def get_describe( screen_name):
 
             # Attach labels to the images
             j=0
+            desc = ""
+            c = 0
             for label in labels:
+                desc = desc + label.description + ','
+                c = c + 1
                 raw_image = Image.open(file_name)
                 draw = ImageDraw.Draw(raw_image)
                 font = ImageFont.truetype('C:/Windows/Fonts/Tahoma/tahoma.ttf', 15) #solved problem: OSError: cannot open resource   ∵ set wrong path to .ttf
@@ -34,7 +47,29 @@ def get_describe( screen_name):
                 j=j+1
                 raw_image.save(file_name)
             i = i+1
+            mtime = datetime.datetime.now()
+            desc = desc[:-1]
+            val = (username, img_num, desc, c, file_name,mtime)
+            mycursor.execute(sql, val)
+            mydb.commit()
 
         else:
             print('no more jpg existed')
             break
+
+    # # insert to mysql db
+    # mydb = mysql.connector.connect(host="localhost", user="root", passwd="123456", auth_plugin='mysql_native_password')
+    # mycursor = mydb.cursor()
+    # mycursor.execute("use mini_project3_db")
+    # sql = "INSERT INTO user_data (username, img_num,description,description_num) VALUES (%s, %s,%s,%s)"
+    # desc = desc[:-1]
+    # val = (username, img_num, desc, c)
+    # mycursor.execute(sql, val)
+    # mydb.commit()
+
+    # insert mongodb
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["mydatabase"]
+    mycol = mydb["users"]
+    mydict = {"username": username, "img_num": img_num, "description": desc, "description_num": c}
+    x = mycol.insert_one(mydict)
--- generatevedio.py
+++ generatevedio.py
@@ -1,11 +1,29 @@
-#import os
+import os
 import subprocess
+import pymongo
+import mysql.connector
+import datetime
 
+def create(screen_name,img_num):
 
-def create(screen_name):
+    # connect database
+    mydb = mysql.connector.connect(host="localhost", user="root", passwd="123456", auth_plugin='mysql_native_password')
+    mycursor = mydb.cursor()
+    mycursor.execute("use mini_project3_db")
+    sql = "INSERT INTO user_data (username,img_num,description,description_num,file_name,Modified_time) VALUES (%s,%s,%s,%s,%s,%s)"
+
     # Convert images with labels into vedio
     try:
         #os.chdir('C:/Users/synox/mini_project1/') change to dirctionary including images
-        subprocess.Popen("ffmpeg -r 1 -i "+screen_name+"/image%d.jpg -vcodec mpeg4 -y "+screen_name+"/"+screen_name+".mp4"" -vf scale=trunc(iw/2)*2:trunc(ih/2)*2")
+        subprocess.Popen("ffmpeg -r 1 -i "+screen_name+"/image%d.jpg -vcodec mpeg4 -y "+screen_name+"/"+screen_name+".avi"" -vf scale=trunc(iw/2)*2:trunc(ih/2)*2")
+        # subprocess.call(['ffmpeg', '-framerate', '1', '-i', screen_name +  '/image%d.jpg', '-vcodec', 'mpeg4', screen_name + '.avi'])
+
+        if os.path.exists('./' + screen_name + '/' + screen_name +'.avi') == True:
+            file_name = 'C:/Users/synox/mini_project1/' + screen_name + '/' + screen_name + '.avi'
+            username = screen_name
+            mtime = datetime.datetime.now()
+            val = (username, img_num, 'N/A', 'N/A', file_name, mtime)
+            mycursor.execute(sql, val)
+            mydb.commit()
     except:
         print('error')
